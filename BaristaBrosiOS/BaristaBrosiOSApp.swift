157c08730367ef2af280f1834697e63977c55a38
tyler <tduic@andrew.cmu.edu>
2021-03-22 21:09:01 -0400

    Initial Commit

---
+++ BaristaBrosiOS/BaristaBrosiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaristaBrosiOSApp.swift
+//  BaristaBrosiOS
+//
+//  Created by Tyler Duic on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct BaristaBrosiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
