e5027c1ae50abf9a03f85f9c0c3080621d36d778
tyler <tduic@andrew.cmu.edu>
2021-03-24 11:02:08 -0400

    first commit

--- BaristaBrosiOS/ContentView.swift
+++ BaristaBrosiOS/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
 struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            WelcomeText()
+            TextField("Username", text: $username)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+            Button(action: {print("Button pressed")}) {
+                LoginButtonContent()
+            }
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WelcomeText: View {
+    var body: some View {
+        Text("Welcome to Barista Bros")
+            .font(.title)
+            .fontWeight(.bold)
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+}
+
+struct LoginButtonContent: View {
+    var body: some View {
+        Text("LOGIN")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.green)
+            .cornerRadius(15.0)
     }
 }
