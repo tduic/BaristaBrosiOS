9d2530bbeb978b2351edbe12e5cf0f60cfe3a1f8
tyler <tduic@andrew.cmu.edu>
2021-03-30 19:09:14 -0400

    remove model file

--- BaristaBrosiOS/Views/ContentView.swift
+++ BaristaBrosiOS/Views/ContentView.swift
@@ -10,20 +10,46 @@ import CoreData
 
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
 
+//class PageView: ObservableObject {
+//    @Published var page = Pages.Login
+//}
+
 struct ContentView: View {
+    
+    @State var page = Pages.Login
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                LoginView()
-                    .navigationTitle("Barista Bros")
-                NavigationLink(
-                    destination: SignupView(),
-                    label: {
-                        SignupButtonContent()
-                    }
-                )
+        switch page {
+        case .Login:
+            NavigationView {
+                VStack {
+                    LoginView()
+                        .navigationTitle("Barista Bros")
+                    NavigationLink(
+                        destination: SignupView(),
+                        label: {
+                            SignupButtonContent()
+                        }
+                    )
+                    .navigationBarBackButtonHidden(true)
+                }
             }
+        default:
+            HomeView()
         }
+//        VStack {
+//            switch page {
+//            case .Login:
+//                LoginView()
+//                Button(action: {page = Pages.Signup}) {
+//                    SignupButtonContent()
+//                }
+//            case .Signup:
+//                SignupView()
+//            default:
+//                LoginView()
+//            }
+//        }
     }
 }
 
@@ -31,4 +57,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+enum Pages : Int {
+    case Login = 0
+    case Signup = 1
+    case Home = 2
 }
