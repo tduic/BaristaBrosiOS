d30e013ed84290dca272f22e32f54b2028781d53
tyler <tduic@andrew.cmu.edu>
2021-05-10 22:56:16 -0400

    need to see diff logs on github

--- BaristaBrosiOS/Model/Drink.swift
+++ BaristaBrosiOS/Model/Drink.swift
@@ -25,9 +25,12 @@ struct Drink: Hashable, Codable {
 extension Drink {
     static var AllDrinks: [Drink] {
         [
-            Drink(name: "Vodka Tonic", alcohols: ["Vodka" : 80], mixers: ["Tonic" : 140], calories: 183, rating: 4, ice: true, imageName: "VT"),
-            Drink(name: "Gin & Tonic", alcohols: ["Gin" : 80], mixers: ["Tonic" : 140], calories: 125, rating: 4, ice: true, imageName: "GT"),
-            Drink(name: "Tequila Sunrise", alcohols: ["Corona" : 130, "Tequila": 40], mixers: ["Blue Curacao" : 10, "Orange Juice": 40], calories: 232, rating: 5, ice: true, imageName: "TS"),
+            Drink(name: "Tequila Sunrise", alcohols: ["Tequila": 40], mixers: ["Orange Juice": 80], calories: 232, rating: 5, ice: true, imageName: "TS"),
+            Drink(name: "Tequila Coke", alcohols: ["Tequila" : 40], mixers: ["Coca Cola": 80], calories: 200, rating: 5, ice: true, imageName: "TC"),
+            Drink(name: "Screwdriver", alcohols: ["Vodka" : 40], mixers: ["Orange Juice": 80], calories: 200, rating: 5, ice: true, imageName: "SD"),
+            Drink(name: "Vodka Coke", alcohols: ["Vodka" : 40], mixers: ["Coca Cola": 80], calories: 200, rating: 5, ice: true, imageName: "VC"),
+            Drink(name: "Vodka Tonic", alcohols: ["Vodka" : 80], mixers: ["Tonic" : 160], calories: 183, rating: 4, ice: true, imageName: "VT"),
+            Drink(name: "Gin & Tonic", alcohols: ["Gin" : 80], mixers: ["Tonic" : 160], calories: 125, rating: 4, ice: true, imageName: "GT"),
             Drink(name: "Screwdriver", alcohols: ["Vodka" : 90], mixers: ["Orange Juice" : 130], calories: 164, rating: 4.5, ice: true, imageName: ""),
             Drink(name: "Moscow Mule", alcohols: ["Vodka" : 50, "Ginger Beer": 120], mixers: ["Lime Juice" : 20], calories: 182, rating: 3.5, ice: true, imageName: ""),
             Drink(name: "Long Island Ice Tea", alcohols: ["Vodka" : 20, "Rum": 20, "Tequila": 20, "Gin": 20], mixers: ["Triple Sec" : 20, "Cola": 40, "Lime Juice": 40], calories: 780, rating: 4, ice: true, imageName: ""),
--- BaristaBrosiOS/Views/CheckLiquidView.swift
+++ BaristaBrosiOS/Views/CheckLiquidView.swift
@@ -10,13 +10,15 @@ import SwiftUI
 struct CheckLiquidView: View {
     
     @Binding var page: Pages
-    @State var liquidNum = "-1"
     
     private let ble = BLEConnection()
     
+//    @State var readValues = ["", "", "", ""]
     @State var readValue = ""
+    @State var readValue1 = ""
     @State var readValue2 = ""
-    @State var timeRemaining = 2
+    @State var readValue3 = ""
+    @State var timeRemaining = 3
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var liquid = Liquid.AllLiquids
@@ -39,26 +41,69 @@ struct CheckLiquidView: View {
                                         ? ble.readValue
                                         : String(Int(ble.readValue[ble.readValue.startIndex].asciiValue!) * 10)
                             }
-                        Button(action: {refillLiquid(liquid: "R", name: "Vodka")}) {
+                        Button(action: {refillLiquid(liquid: "V", name: "Vodka")}) {
                             RefillButtonContent()
                         }
+                        
                         Spacer()
                             .frame(height: 25)
                 
                         liquid[1].image
                             .resizable()
                             .frame(width:80, height: 80)
-                        Text(verbatim: liquid[1].name)
+                        Text(liquid[1].name)
+                        Text("\(readValue1)")
+                            .onReceive(timer) {
+                                input in
+                                readValue1 = timeRemaining > 0
+                                    ? "Checking values..."
+                                    : ble.readValue == ""
+                                        ? ble.readValue
+                                        : String(Int(ble.readValue[ble.readValue.index(ble.readValue.startIndex, offsetBy: 1)].asciiValue!) * 10)
+                            }
+                        Button(action: {refillLiquid(liquid: "T", name: "Tequila")}) {
+                            RefillButtonContent()
+                        }
+                    }
+                    
+                    Spacer()
+                        .frame(width: 30)
+                    
+                    VStack {
+                        liquid[2].image
+                            .resizable()
+                            .frame(width:80, height: 80)
+                        Text(liquid[2].name)
                         Text("\(readValue2)")
                             .onReceive(timer) {
                                 input in
                                 readValue2 = timeRemaining > 0
                                     ? "Checking values..."
                                     : ble.readValue == ""
+                                        ? "ble.readValue"
+                                        : String(Int(ble.readValue[ble.readValue.index(ble.readValue.startIndex, offsetBy: 2)].asciiValue!) * 10)
+                            }
+                        Button(action: {refillLiquid(liquid: "C", name: "Coca Cola")}) {
+                            RefillButtonContent()
+                        }
+                        
+                        Spacer()
+                            .frame(height: 25)
+                
+                        liquid[3].image
+                            .resizable()
+                            .frame(width:80, height: 80)
+                        Text(liquid[3].name)
+                        Text("\(readValue3)")
+                            .onReceive(timer) {
+                                input in
+                                readValue3 = timeRemaining > 0
+                                    ? "Checking values..."
+                                    : ble.readValue == ""
                                         ? ble.readValue
                                         : String(Int(ble.readValue[ble.readValue.index(before: ble.readValue.endIndex)].asciiValue!) * 10)
                             }
-                        Button(action: {refillLiquid(liquid: "r", name: "Red Bull")}) {
+                        Button(action: {refillLiquid(liquid: "O", name: "Orange Juice")}) {
                             RefillButtonContent()
                         }
                     }
@@ -88,12 +133,11 @@ struct CheckLiquidView: View {
     }
     
     func checkLiquid() {
-        let check = "C: check liquids"
+        let check = "L"
         ble.bleWriteCharacteristic(uuid: ble.uuidHM10Char, data: check.data(using: .utf8) ?? Data())
     }
     
     func refillLiquid(liquid: String, name: String) {
-        liquidNum = liquid
         ble.bleWriteCharacteristic(uuid: ble.uuidHM10Char, data: liquid.data(using: .utf8) ?? Data())
     }
 }
